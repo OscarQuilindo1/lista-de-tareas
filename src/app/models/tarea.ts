d0cd21ae44cbc4231faabbef7dbc0c63aff6b632
OscarQuilindo1 <oscarquilindo@unicomfacauca.edu.co>
2023-01-17 12:36:14 -0500

    first commit

---
+++ src/app/models/tarea.ts
@@ -0,0 +1,10 @@
+export class Tarea{
+    nombre:string;
+    estado:boolean;
+
+    constructor(nombre1:string, estado1:boolean){
+        this.estado = estado1;
+        this.nombre = nombre1;
+    }
+
+}
